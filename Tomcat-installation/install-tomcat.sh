70b6cb0cfc67247412a702f54bbb7802bb98518f
assumagbams <105608304+assumagbams@users.noreply.github.com>
2022-12-03 15:37:14 -0500

    Update install-tomcat.sh

--- Tomcat-installation/install-tomcat.sh
+++ Tomcat-installation/install-tomcat.sh
@@ -11,10 +11,10 @@ sudo yum install java-1.8.0-openjdk-devel -y
 # Download tomcat software and extract it.
 sudo yum install wget unzip -y
 
-sudo wget https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.62/bin/apache-tomcat-9.0.62.tar.gz
-sudo tar -xvf apache-tomcat-9.0.62.tar.gz
-sudo rm apache-tomcat-9.0.62.tar.gz
-sudo mv apache-tomcat-9.0.62 tomcat9
+sudo wget https://archive.apache.org/dist/tomcat/tomcat-9/v9.0.0.M10/bin/apache-tomcat-9.0.0.M10.tar.gz
+sudo tar -xvf apache-tomcat-9.0.0.M10 tar.gz
+sudo rm apache-tomcat-9.0.0.M10.tar.gz
+sudo mv apache-tomcat-9.0.0.M10 tomcat9
 sudo chmod 777 -R /opt/tomcat9
 sudo chown ec2-user -R /opt/tomcat9
 sh /opt/tomcat9/bin/startup.sh
@@ -31,8 +31,16 @@ echo "end on tomcat installation"
 
 #Tomcat server configuration:
 find / -name server.xml context.xml
-vim /opt/tomcat9/conf/server.xml
+vim /opt/tomcat9/conf/server.xml #for port configuration
+
+#insert openign and closing tag on the part below after 
 vi /opt/tomcat9/webapps/manager/META-INF/context.xml
+comment out this file part
+<!--
+ <Valve className="org.apache.catalina.valves.RemoteAddrValve"
+         allow="127\.\d+\.\d+\.\d+|::1|0:0:0:0:0:0:0:1" />
+ -->
+#
 vi /opt/tomcat9/conf/tomcat-user.xml  # to add user
 
 	<user username="landmark" password="admin" roles="manager-gui,admin-gui"/>
